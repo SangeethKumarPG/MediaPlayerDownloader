a7b22b9fde7a5a4f5d3eb170fa418c3742966073
SangeethKumarPG <101401368+SangeethKumarPG@users.noreply.github.com>
2024-08-19 14:03:23 +0530

    Update index.js

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const app = express();
 const port = 3000;
 
 app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "https://main--youtubemediadownloader.netlify.app");
+  res.header("Access-Control-Allow-Origin", "https://mediaplayerfrontend.onrender.com");
   res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
   res.header("Access-Control-Allow-Headers", "Content-Type");
   next();
